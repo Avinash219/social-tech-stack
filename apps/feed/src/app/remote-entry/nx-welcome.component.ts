d905f9203af897ca0628bc0df37d31879ddf4f12
Avinash219 <avinash.dev21987@gmail.com>
2024-08-07 21:53:41 +0530

    Updated yml

--- apps/feed/src/app/remote-entry/nx-welcome.component.ts
+++ apps/feed/src/app/remote-entry/nx-welcome.component.ts
@@ -7,7 +7,7 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: ` Test Feed for affected apps updated andverified and upd and again
   ate and test update and again and again and again and again and again and
-  again`,
+  again and again`,
   styles: [],
   encapsulation: ViewEncapsulation.None,
 })
