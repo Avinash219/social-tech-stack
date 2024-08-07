249824ab030b966fda4c0aa7b4580c70ddcccb01
Avinash219 <avinash.dev21987@gmail.com>
2024-08-07 22:45:42 +0530

    Updated yml

--- apps/projects/src/app/remote-entry/nx-welcome.component.ts
+++ apps/projects/src/app/remote-entry/nx-welcome.component.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
   selector: 'app-nx-welcome',
   standalone: true,
   imports: [CommonModule],
-  template: ` Updated yml changes `,
+  template: ` Testing for yml changes`,
   styles: [],
   encapsulation: ViewEncapsulation.None,
 })
